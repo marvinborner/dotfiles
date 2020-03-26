4c744f6c96499a62ad015ba808accc4a0b4e1f6d
Marvin Borner <develop@marvinborner.de>
2020-03-26 13:09:48 +0100

    Added network check

--- .repos/dwm/config.h
+++ .repos/dwm/config.h
@@ -3,7 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
@@ -26,9 +26,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class          instance    title       tags mask     isfloating   monitor */
-	{ "QuteBrowser",  NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "Spotify",      NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class		  instance    title       tags mask     isfloating   monitor */
+	{ "qutebrowser",	  NULL,       NULL,       1 << 0,       0,           -1 },
+	{ "Spotify",		  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "TelegramDesktop",      NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Signal",	          NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "discord",		  NULL,       NULL,       1 << 7,       0,           -1 },
+	{ NULL,			  NULL,       "WhatsApp", 1 << 7,       0,           -1 },
 };
 
 /* layout(s) */
